60ce575b231fb7cfdf7bd1ba555b5fbdeac5b8ed
Raul Pele <raul.pele2001@gmail.com>
2022-07-27 10:12:09 +0300

    Initial Commit

---
+++ FlagsGenerator/FlagsGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsGeneratorApp.swift
+//  FlagsGenerator
+//
+//  Created by Raul Pele on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagsGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
