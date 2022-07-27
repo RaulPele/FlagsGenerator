231b9f11687b0d90a954f26363c1838026a40515
Raul Pele <raul.pele2001@gmail.com>
2022-07-27 17:05:28 +0300

    created structs for top and bottom view sections

--- FlagsGenerator/FlagView.swift
+++ FlagsGenerator/FlagView.swift
@@ -11,28 +11,30 @@ struct FlagView: View {
     var body: some View {
         
         VStack(spacing: 0) {
-//            VStack(spacing: 0) {
-//                HStack {
-//                    Spacer()
-//                    
-//                    Button {
-//                        
-//                    } label: {
-//                        Text("Save Flag")
-//                            .padding(.horizontal, 15)
-//                            .padding(.vertical, 9)
-//                    }
-//                    .background(Color(red: 0.131, green: 0.573, blue: 0.712)
-//                        .opacity(0.2))
-//                    .clipShape(RoundedRectangle(cornerRadius: 8))
-//                }
-//                .padding(.trailing)
-//            }
-//            
-//            VStack{
-//                Color(red: 0.865, green: 0.883, blue: 0.88)
-//                    .ignoresSafeArea()
-//            }
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+
+                    Button {
+
+                    } label: {
+                        Text("Save Flag")
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 9)
+                    }
+                    .background(Color(red: 0.131, green: 0.573, blue: 0.712)
+                        .opacity(0.2))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                .padding(.trailing)
+            }
+
+            VStack(spacing: 0){
+                Color(red: 0.865, green: 0.883, blue: 0.88)
+                    .ignoresSafeArea()
+            }
+            .padding(.horizontal, 85)
+            .padding(.vertical, 45)
         }
     }
 }
